9461fd297dc02183ae61322a2af87f48f058226e
cannnnnnnn <cnigro@pioix.edu.ar>
2024-11-12 17:08:48 +0100

    footer
    sin terminar

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import './Footer.module.css';  // Estilos para el footer
 
-const Footer = () => (
-  <footer className="bg-light p-3 text-center" data-testid="footer">
-    <div className="logo" data-testid="footer-logo" />
-    <p data-testid="footer-text">
-      Sample project provided by <a href="https://auth0.com">Auth0</a>
-    </p>
-  </footer>
-);
+const Footer = () => {
+  return (  
+    <footer className="footer">
+      <div className="footer-content">
+        {/* Logo y nombre */}
+        <div className="footer-logo">
+          <img src="/logo-owl.png" alt="Logo de Owl" className="footer-logo-img" />
+          <p className="footer-name">Owl Social Network</p>
+        </div>
+
+        {/* Enlaces principales */}
+        <div className="footer-links">
+          <ul>
+            <li><a href="/acerca-de">Acerca de</a></li>
+            <li><a href="/terminos-y-condiciones">Términos y Condiciones</a></li>
+            <li><a href="/privacidad">Privacidad</a></li>
+            <li><a href="/faq">FAQ</a></li>
+            <li><a href="/contacto">Contacto</a></li>
+          </ul>
+        </div>
+
+        {/* Redes sociales */}
+        <div className="footer-socials">
+          <a href="https://twitter.com/owl" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="https://instagram.com/owl" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://facebook.com/owl" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-facebook"></i>
+          </a>
+        </div>
+      </div>
+
+      {/* Copyright */}
+      <div className="footer-copyright">
+        <p>© 2024 Owl. Todos los derechos reservados.</p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
