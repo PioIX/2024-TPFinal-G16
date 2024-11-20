e0db77c7a77044e00a479fcfd8f6e183eca6b2ce
tomydb <163529016+tomydb@users.noreply.github.com>
2024-11-20 09:48:35 -0300

    Navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -64,129 +64,124 @@ const NavBar = () => {
               {user && (
                 <>
                   <NavItem>
-                    <div onClick={handleImageHomeChange} style={{ cursor: 'pointer', display: 'inline-block' }}>
-                      <PageLink href="/csr" className="nav-link" testId="navbar-csr">
-                          <img src={imageHome} width={"57px"} height={"57px"}></img>
+                    <div onClick={handleImageHomeChange} style={{ cursor: 'pointer', display: 'inline-block'}}>
+                      <PageLink href="/csr" className="nav-link p-0 m-0" testId="navbar-csr">
+                          <img src={imageHome} width={"45px"} height={"45px"}></img>
                       </PageLink>
                     </div>
                   </NavItem>
                   <NavItem>
                     <div onClick={handleImageLupaChange} style={{ cursor: 'pointer', display: 'inline-block' }}>
-                      <PageLink href="/external" className="nav-link" testId="navbar-external">
-                          <img src={imageLupa} width="57px" height="57px"/>
+                      <PageLink href="/external" className="nav-link p-0 m-0" testId="navbar-external">
+                          <img src={imageLupa} width="45px" height="45px"/>
                       </PageLink>
                     </div>
                   </NavItem>
                   <img src='./images/logo.png' width={"200px"} height={"130px"}></img>
                   <NavItem>
-                    <div onClick={handleImageNotificationChange} style={{ cursor: 'pointer', display: 'inline-block' }}>
-                      <PageLink href="/ssr" className="nav-link" testId="navbar-ssr">
-                          <img src={imageNotification} width={"64px"} height={"64px"}></img>
-                      </PageLink>
-                    </div>
-                  </NavItem>
-                  <NavItem>
                     <div onClick={handleImageChatChange} style={{ cursor: 'pointer', display: 'inline-block' }}>
-                      <PageLink href="/external" className="nav-link" testId="navbar-external">
-                        <img src={imageChat} width={"57px"} height={"57px"}></img>
+                      <PageLink href="/external" className="nav-link p-0 m-0" testId="navbar-external">
+                        <img src={imageChat} width={"45px"} height={"45px"}></img>
                       </PageLink>
                     </div>
                   </NavItem>
                 </>
               )}
-            </Nav>
-            <Nav className="d-none d-md-block" navbar>
+                <Nav className="d-none d-md-block" style={{display:"flex", alignContent: "center", alignItems:"center"}} navbar>
+                {!isLoading && !user && (
+                  <NavItem id="qsLoginBtn">
+                    <AnchorLink
+                      href="/api/auth/login"
+                      className="btn btn-primary btn-margin"
+                      tabIndex={0}
+                      testId="navbar-login-desktop">
+                      Log in
+                    </AnchorLink>
+                  </NavItem>
+                )}
+                {user && (
+                  <UncontrolledDropdown nav inNavbar data-testid="navbar-menu-desktop" >
+                    <DropdownToggle style={{display:"flex", verticalAlign:"middle", alignContent: "center", alignItems:"center"}}nav caret id="profileDropDown">
+                      <img
+                        src={user.picture}
+                        alt="Profile"
+                        className="nav-user-profile rounded-circle"
+                        verticalAlign="middle"
+                        width="60"
+                        height="60"
+                        decode="async"
+                        data-testid="navbar-picture-desktop"
+                      />
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem header data-testid="navbar-user-desktop">
+                        {user.name}
+                      </DropdownItem>
+                      <DropdownItem className="dropdown-profile" tag="span">
+                        <PageLink href="/profile" icon="user" testId="navbar-profile-desktop">
+                          Profile
+                        </PageLink>
+                      </DropdownItem>
+                      <DropdownItem id="qsLogoutBtn">
+                        <AnchorLink href="/api/auth/logout" icon="power-off" testId="navbar-logout-desktop">
+                          Log out
+                        </AnchorLink>
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                )}
+              </Nav>
               {!isLoading && !user && (
-                <NavItem id="qsLoginBtn">
+                <Nav className="d-md-none" navbar>
                   <AnchorLink
                     href="/api/auth/login"
-                    className="btn btn-primary btn-margin"
+                    className="btn btn-primary btn-block"
                     tabIndex={0}
-                    testId="navbar-login-desktop">
+                    testId="navbar-login-mobile">
                     Log in
                   </AnchorLink>
-                </NavItem>
+                </Nav>
               )}
               {user && (
-                <UncontrolledDropdown nav inNavbar data-testid="navbar-menu-desktop">
-                  <DropdownToggle nav caret id="profileDropDown">
-                    <img
-                      src={user.picture}
-                      alt="Profile"
-                      className="nav-user-profile rounded-circle"
-                      width="57"
-                      height="57"
-                      decode="async"
-                      data-testid="navbar-picture-desktop"
-                    />
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem header data-testid="navbar-user-desktop">
-                      {user.name}
-                    </DropdownItem>
-                    <DropdownItem className="dropdown-profile" tag="span">
-                      <PageLink href="/profile" icon="user" testId="navbar-profile-desktop">
-                        Profile
-                      </PageLink>
-                    </DropdownItem>
-                    <DropdownItem id="qsLogoutBtn">
-                      <AnchorLink href="/api/auth/logout" icon="power-off" testId="navbar-logout-desktop">
-                        Log out
-                      </AnchorLink>
-                    </DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
+                <Nav
+                  id="nav-mobile"
+                  className="d-md-none justify-content-between"
+                  navbar
+                  data-testid="navbar-menu-mobile">
+                  <NavItem style={{verticalAlign: "middle"}}>
+                    <span className="user-info">
+                      <img
+                        src={user.picture}
+                        alt="Profile"
+                        className="nav-user-profile d-inline-block rounded-circle"
+                        width="55"
+                        height="55"
+                        decode="async"
+                        data-testid="navbar-picture-mobile"
+                        verticalAlign="middle"
+                      />
+                      <h6 className="d-inline-block" data-testid="navbar-user-mobile">
+                        {user.name}
+                      </h6>
+                    </span>
+                  </NavItem>
+                  <NavItem>
+                    <PageLink href="/profile" icon="user" testId="navbar-profile-mobile">
+                      Profile
+                    </PageLink>
+                  </NavItem>
+                  <NavItem id="qsLogoutBtn">
+                    <AnchorLink
+                      href="/api/auth/logout"
+                      className="btn btn-link p-0"
+                      icon="fa-solid fa-power-off"
+                      testId="navbar-logout-mobile">
+                      Log out
+                    </AnchorLink>
+                  </NavItem>
+                </Nav>
               )}
             </Nav>
-            {!isLoading && !user && (
-              <Nav className="d-md-none" navbar>
-                <AnchorLink
-                  href="/api/auth/login"
-                  className="btn btn-primary btn-block"
-                  tabIndex={0}
-                  testId="navbar-login-mobile">
-                  Log in
-                </AnchorLink>
-              </Nav>
-            )}
-            {user && (
-              <Nav
-                id="nav-mobile"
-                className="d-md-none justify-content-between"
-                navbar
-                data-testid="navbar-menu-mobile">
-                <NavItem>
-                  <span className="user-info">
-                    <img
-                      src={user.picture}
-                      alt="Profile"
-                      className="nav-user-profile d-inline-block rounded-circle mr-3"
-                      width="50"
-                      height="50"
-                      decode="async"
-                      data-testid="navbar-picture-mobile"
-                    />
-                    <h6 className="d-inline-block" data-testid="navbar-user-mobile">
-                      {user.name}
-                    </h6>
-                  </span>
-                </NavItem>
-                <NavItem>
-                  <PageLink href="/profile" icon="user" testId="navbar-profile-mobile">
-                    Profile
-                  </PageLink>
-                </NavItem>
-                <NavItem id="qsLogoutBtn">
-                  <AnchorLink
-                    href="/api/auth/logout"
-                    className="btn btn-link p-0"
-                    icon="fa-solid fa-power-off"
-                    testId="navbar-logout-mobile">
-                    Log out
-                  </AnchorLink>
-                </NavItem>
-              </Nav>
-            )}
           </Collapse>
         </Container>
       </Navbar>
