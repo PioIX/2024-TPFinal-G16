0cd32ac5860ad1379775c8f88cfe70293688c13c
Joaquin-Colodro <163159110+Joaquin-Colodro@users.noreply.github.com>
2024-11-20 15:50:45 +0100

    Funko

---
+++ components/InputChat.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import styles from './Input.module.css';
+
+export default function Input({ placeholder }) {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    return (
+        <div>
+            <input
+                className={styles.Input}
+                value={inputValue}
+                onChange={handleChange}
+                placeholder={placeholder} // Utiliza el placeholder
+            />
+            <button className={styles.postButton}>
+            Enviar
+            </button>
+        </div>
+    );
+}
